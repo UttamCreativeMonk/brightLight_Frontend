784e6acd4da5d091dbb43088f5da1296416beb4f
UttamCreativeMonk <uttam.creativemonk@gmail.com>
2024-10-14 14:57:35 +0530

    MetaOf_8PagesDone

--- src/pages/FederalSkilledWorkerProgram.jsx
+++ src/pages/FederalSkilledWorkerProgram.jsx
@@ -6,10 +6,12 @@ import Footer1 from "../components/Footer1";
 import Testimonials from "../sections/Testimonials";
 import RecentBlogs from "../sections/RecentBlogs";
 import FAQ from "../sections/FAQ";
+import ogImage from "../assets/ogImage.png";
+import { Helmet } from "react-helmet-async";
 
 const FederalSkilledWorkerProgram = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
+  let [metaData, setMetaData] = useState([]);
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
@@ -20,6 +22,24 @@ const FederalSkilledWorkerProgram = () => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+
+  useEffect(() => {
+
+    fetch("https://brightlight-node.onrender.com/federalSkillWorkerProgMeta")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data) {
+          setMetaData(data[0]);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
 
   const sectionsRef = useRef([]);
 
@@ -45,6 +65,48 @@ const FederalSkilledWorkerProgram = () => {
 
   return (
     <>
+            <Helmet>
+        <title>
+          {metaData?.metaTitle
+            ? metaData?.metaTitle
+            : "Brightlight Immigration"}
+        </title>
+        <meta
+          name="description"
+          content={
+            metaData?.metaDesc
+              ? metaData?.metaDesc
+              : "Learn about Brightlight Immigration, our mission, values, and the dedicated team behind our immigration services. We are committed to providing honest and accurate advice to guide you through your immigration journey."
+          }
+        />
+        <meta
+          name="title"
+          property="og:title"
+          content={
+            metaData?.metaOgTitle
+              ? metaData?.metaOgTitle
+              : " Brightlight Immigration"
+          }
+        />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:image:type" content="image/png" />
+        <meta
+          property="og:description"
+          content={
+            metaData?.metaOgDesc
+              ? metaData?.metaOgDesc
+              : "Discover the story behind Brightlight Immigration, our commitment to providing honest and accurate advice, and how our team can assist you with your immigration needs."
+          }
+        />
+        <meta
+          name="Keywords"
+          content={
+            metaData?.metaKeywords
+              ? metaData?.metaKeywords
+              : "Brightlight Immigration, Immigration Services, Mission, Team"
+          }
+        />
+      </Helmet>
       <Navbar1 />
       <div className={styles.bannerParent}>
         <div className={styles.banner}>
